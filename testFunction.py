fb06dada926271e0d702e71231b756380cb6db89
mainPut <1663969122@qq.com>
2024-01-16 16:57:36 +0800

    第二次提交

--- testFunction.py
+++ testFunction.py
@@ -1,11 +1,27 @@
 import cv2
 from paddleocr import PaddleOCR,draw_ocr
-ocr = PaddleOCR()
-img = cv2.imread("B.png")
+import requests
+from bs4 import BeautifulSoup
+#输出图片文字
+def FindStr(imgPath):
+    ocr = PaddleOCR()
+    img = cv2.imread("CONNECT.png")
 
-result = ocr.ocr(img,cls=False)
+    result = ocr.ocr(img, cls=False)
 
-for idx in range(len(result)):
-    res = result[idx]
-    for line in res:
-        print(line)
+    for line in result:
+        for i in line:
+            return i[-1][0]
+
+url = "https://game.cryptounicorns.fun"
+print(url)
+response = requests.get(url)
+if response.status_code==200:
+    pass
+else:
+    print("请求错误")
+
+html = response.content
+soup = BeautifulSoup(html,'html.parser')
+element = soup.find('div',{'class':'sc-imWYAI iZlvRU'})
+print(element)
